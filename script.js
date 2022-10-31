ee4de7b4ef0d2d0c24aa85b1a82f8bbf54e9283e
MrBurp <andreass.ziegler@gmail.com>
2022-10-31 12:43:51 +0100

    func. writeText

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const textEl = document.getElementById("text");
 const speedEL = document.getElementById("speed");
 const text = "Programming is more exciting than  playing Playstation 5";
+
+let idx = 1;
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+}
